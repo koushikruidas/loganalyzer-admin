78bc5dfd81fbef829e7ce6bc47e9ed53ced5c851
Koushik Ruidas <koushikruidas@gmail.com>
2025-09-01 13:22:14 +0530

    fixed cors issue and swagger issue

--- src/main/java/com/autumn/loganalyzer_admin/config/OpenApi30Config.java
+++ src/main/java/com/autumn/loganalyzer_admin/config/OpenApi30Config.java
@@ -33,7 +33,7 @@ public class OpenApi30Config {
     return new OpenAPI()
         .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
         .servers(List.of(
-            new Server().url("http://logpulse.dev/api/loganalyzer-admin"),
+            new Server().url("https://logpulse.dev/api/loganalyzer-admin"),
             new Server().url("http://localhost:8080/loganalyzer-admin")
         ))
         .components(
